b5b51c7537b8cd8ad9a54c9eff787cea4e111f70
StormTechCanaa16054 <fehdacostaquintanilhafilho@gmail.com>
2024-10-19 10:48:44 -0300

    homepage 85%

--- storm-website/src/App.tsx
+++ storm-website/src/App.tsx
@@ -1,15 +1,16 @@
 import pantera from './assets/kira-roxa.png';
 import './App.css'; // Import the CSS file
 import videostorm from './assets/storm-thumb.mp4';
+import YouTube from 'react-youtube';
 
 export default function App() {
   return (
     <nav>
       <div className="flex justify-between items-center">
         <div className="flex items-center">
-          <img className="w-32 h-28 ml-7 mt-5" src={pantera} alt="Logotipo da empresa" />
+          <img className="w-32 h-28 ml-7 mt-5" src={pantera} alt="Kira" />
         </div>
-        <ul className="text-4xl flex gap-10 text-purple-600 font-black mr-32">
+        <ul className="text-4xl flex gap-10 text-roxo font-black mr-32">
           <li className="border-dashed border-purple-600 hover:border-b-2 hover:border-solid hover:border-purple-800">HOME</li>
           <li className="border-dashed border-purple-600 hover:border-b-2 hover:border-solid hover:border-purple-800">FIRST</li>
           <li className="border-dashed border-purple-600 hover:border-b-2 hover:border-solid hover:border-purple-800">ABOUT US</li>
@@ -22,6 +23,15 @@ export default function App() {
           <source src={videostorm} />
         </video>
       </div>
+      <span className='flex items-center justify-center gap-8 font-bold'>
+        <h1 className='text-9xl'>IN THE</h1>
+        <h1 className='text-9xl text-roxo'>NEWS!</h1>
+      </span>
+      <div className='relative flex items-center justify-center overflow-hidden gap-28 mt-20 '>
+        <YouTube videoId='https://youtu.be/zSTSgdeaSRM' />
+        <YouTube videoId='https://www.youtube.com/watch?v' />
+      </div>
+      <div className="bg-roxo absolute w-full h-96 mt-24"></div>
     </nav>
-  );
+  );  
 }
--- storm-website/tailwind.config.js
+++ storm-website/tailwind.config.js
@@ -3,16 +3,33 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    
+
   ],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-
-  colors: {
-    "roxo-storm": "7600C0",
+    colors: {
+      'blue': '#1fb6ff',
+      'roxo': '#7600C0',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
 }
-
 
